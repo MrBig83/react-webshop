82d6cbe88b0a638f0eb34a6337bf222e9485d341
MrBig83 <mjohansson176@gmail.com>
2023-05-25 16:42:01 +0200

    Lättare styling gjord

--- client/src/assets/components/Main/Main.tsx
+++ client/src/assets/components/Main/Main.tsx
@@ -4,15 +4,21 @@ import SingleProduct from "../SingleProduct/SingleProduct";
 import Products from "../Products/Products";
 import ShopingCart from "../ShopingCart/ShopingCart";
 import Contact from "../Contact/Contact";
+import "./Main.css"
 const Main = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Products />} />
-      <Route path="/:id" element={<SingleProduct />} />
-      <Route path="/shopingcart" element={<ShopingCart />} />
-      <Route path="/kontakta-oss" element={<Contact />} />
+    <div className="main">
+      <div className="routes">
+        <Routes>
+          <Route path="/" element={<Products />} />
+          <Route path="/:id" element={<SingleProduct />} />
+          <Route path="/shopingcart" element={<ShopingCart />} />
+          <Route path="/kontakta-oss" element={<Contact />} />
 
-    </Routes>
+        </Routes>
+        <div className="sectionLine"></div>
+      </div>
+    </div>
   );
 };
 export default Main;
