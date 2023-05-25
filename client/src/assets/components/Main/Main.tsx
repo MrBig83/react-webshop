4736f0b4aa669279933b92c6e148f97a3f4a0d5d
MrBig83 <mjohansson176@gmail.com>
2023-05-25 15:38:58 +0200

    Kontaktsida och styling

--- client/src/assets/components/Main/Main.tsx
+++ client/src/assets/components/Main/Main.tsx
@@ -3,12 +3,15 @@ import { Route, Routes } from "react-router";
 import SingleProduct from "../SingleProduct/SingleProduct";
 import Products from "../Products/Products";
 import ShopingCart from "../ShopingCart/ShopingCart";
+import Contact from "../Contact/Contact";
 const Main = () => {
   return (
     <Routes>
       <Route path="/" element={<Products />} />
       <Route path="/:id" element={<SingleProduct />} />
       <Route path="/shopingcart" element={<ShopingCart />} />
+      <Route path="/kontakta-oss" element={<Contact />} />
+
     </Routes>
   );
 };
