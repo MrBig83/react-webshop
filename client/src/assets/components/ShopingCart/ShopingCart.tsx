57de438a72f01fe18eefceeaaa352970603bec0b
Your name hereOscarwarmark <oscar.warmark@gmail.com>
2023-05-25 14:55:52 +0200

    Shopingcart drawer

---
+++ client/src/assets/components/ShopingCart/ShopingCart.tsx
@@ -0,0 +1,12 @@
+const ShopingCart = () => {
+  return (
+    <>
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+      hej
+    </>
+  );
+};
+
+export default ShopingCart;
