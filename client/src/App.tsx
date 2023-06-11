c62821485ca164dafb9237458e72574e220f6e57
Therese Otterhäll <tessan.otterhall@gmail.com>
2023-06-11 08:16:02 +0200

    karusell bara på startsidan

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Layout } from "antd";
+import { useLocation } from "react-router-dom";
 import Main from "./assets/components/Main/Main";
 import Header from "./assets/components/Header/Header";
 import Footer from "./assets/components/Footer/Footer";
@@ -8,22 +9,25 @@ import NavCarousel from "./assets/components/NavCarousel/NavCarousel";
 import CartProvider from "./context/CartContext";
 import UserContextProvider from "./context/UserContext";
 import OrderContextProvider from "./context/OrderContext";
-import ProductContextProvider from "./context/ProductContext"
+import ProductContextProvider from "./context/ProductContext";
 const App: React.FC = () => {
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+
   return (
     <ProductContextProvider>
-    <OrderContextProvider>
-      <UserContextProvider>
-        <CartProvider>
-          <Layout className="layout">
-            <Header />
-            <NavCarousel />
-            <Main />
-            <Footer />
-          </Layout>
-        </CartProvider>
-      </UserContextProvider>
-    </OrderContextProvider>
+      <OrderContextProvider>
+        <UserContextProvider>
+          <CartProvider>
+            <Layout className="layout">
+              <Header />
+              {isHomePage && <NavCarousel />}
+              <Main />
+              <Footer />
+            </Layout>
+          </CartProvider>
+        </UserContextProvider>
+      </OrderContextProvider>
     </ProductContextProvider>
   );
 };
