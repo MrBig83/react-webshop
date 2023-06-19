f995e032f56215aa7a21551dcf082b8bd308f386
Your name hereOscarwarmark <oscar.warmark@gmail.com>
2023-06-19 22:31:57 +0200

    fix av kundvagns check

--- client/src/assets/components/Checkout/Checkout.tsx
+++ client/src/assets/components/Checkout/Checkout.tsx
@@ -1,21 +1,27 @@
 import CheckoutForm from "../CheckoutForm/CheckoutForm";
 import ShopingCart from "../ShopingCart/ShopingCart";
 import { UserContext } from "../../../context/UserContext";
+import { MyCartContext } from "../../../context/CartContext";
 import { useContext } from "react";
 
 import "./Checkout.css";
 const Checkout = () => {
   const { data } = useContext(UserContext);
+  const { items } = useContext(MyCartContext);
+  console.log(items);
+
   if (!data._id) {
     return <h1>För att lägga en order behöver du logga in</h1>;
-  }
-  return (
-    <>
-      <h1>Kassa</h1>
-      <ShopingCart />
-      <CheckoutForm />
-    </>
-  );
+  } else if (!items.length) {
+    return <h1>Inga varor i kundvagnen</h1>;
+  } else
+    return (
+      <>
+        <h1>Kassa</h1>
+        <ShopingCart />
+        <CheckoutForm />
+      </>
+    );
 };
 
 export default Checkout;
