5e5db429aab63bf056648f13d387ff4fc0f353fd
Your name hereOscarwarmark <oscar.warmark@gmail.com>
2023-06-19 22:04:36 +0200

    Fixat kassa url

--- client/src/assets/components/Checkout/Checkout.tsx
+++ client/src/assets/components/Checkout/Checkout.tsx
@@ -1,9 +1,14 @@
 import CheckoutForm from "../CheckoutForm/CheckoutForm";
 import ShopingCart from "../ShopingCart/ShopingCart";
-import OrderContextProvider from "../../../context/OrderContext";
+import { UserContext } from "../../../context/UserContext";
+import { useContext } from "react";
 
 import "./Checkout.css";
 const Checkout = () => {
+  const { data } = useContext(UserContext);
+  if (!data._id) {
+    return <h1>För att lägga en order behöver du logga in</h1>;
+  }
   return (
     <>
       <h1>Kassa</h1>
